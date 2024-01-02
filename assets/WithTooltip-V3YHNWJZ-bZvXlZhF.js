3d7fdf14b34ec7fc271b9caf5a336c6a0be77c85
Dannyel Kayke <dannyel_kayke@hotmail.com>
2024-01-02 11:58:37 -0300

    Updates

---
+++ assets/WithTooltip-V3YHNWJZ-bZvXlZhF.js
@@ -0,0 +1,1 @@
+import{W as h,W as l,a as s}from"./index-i8i5C9v1.js";import"./iframe-PXlZ_eQR.js";import"../sb-preview/runtime.js";import"./index-XiNr8FW2.js";import"./_commonjsHelpers-5-cIlDoe.js";import"./index-9vG4XYWr.js";import"./index-ogXoivrg.js";import"./index-mLPG47JP.js";import"./index-PPLHz8o0.js";export{h as WithToolTipState,l as WithTooltip,s as WithTooltipPure};
---
+++ assets/configure--na3o5NW.js
@@ -0,0 +1,36 @@
+import{j as e}from"./jsx-runtime-AgcCsxC8.js";import{M as i}from"./index-i8i5C9v1.js";import{useMDXComponents as t}from"./index-7MmEg4M7.js";import"./index-XiNr8FW2.js";import"./_commonjsHelpers-5-cIlDoe.js";import"./iframe-PXlZ_eQR.js";import"../sb-preview/runtime.js";import"./index-9vG4XYWr.js";import"./index-ogXoivrg.js";import"./index-mLPG47JP.js";import"./index-PPLHz8o0.js";function o(r){const n=Object.assign({h1:"h1",h2:"h2",pre:"pre",code:"code"},t(),r.components);return e.jsxs(e.Fragment,{children:[e.jsx(i,{title:"Configure your project"}),`
+`,e.jsxs("div",{className:"sb-container",children:[e.jsx("div",{className:"sb-section-logo",children:e.jsx("img",{src:"./../src/assets/images/logo-celestial.png"})}),e.jsx("div",{className:"sb-separator"}),e.jsx(n.h1,{id:"configure-your-project",children:"Configure your project"}),e.jsx("p",{children:"Celestial Components is a frontend web component library built with React and Styled Components."}),e.jsx(n.h2,{id:"installation",children:"Installation"}),e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-bash",children:`npm install celestial-components
+`})}),e.jsx(n.h2,{id:"usage",children:"Usage"}),e.jsx("p",{children:"Import the components you need in your React application:"}),e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-typescript",children:`import { Foo } from 'celestial-components'
+
+function App() {
+  return (
+    <div>
+      <Foo />
+    </div>
+  )
+}
+`})})]}),`
+`,e.jsx("style",{children:`
+  .sb-container {
+    margin-bottom: 3rem;
+  }
+
+  img {
+    object-fit: cover;
+  }
+
+  .sb-container p {
+    font-size: 0.875rem;
+  }
+
+  .sb-separator {
+    height:0.3rem;
+    border-radius: 100%;
+    margin: 1.2rem 0;
+    background: rgb(107,0,249);
+    background: -moz-linear-gradient(90deg, rgba(107,0,249,1) 0%, rgba(1,99,255,1) 100%);
+    background: -webkit-linear-gradient(90deg, rgba(107,0,249,1) 0%, rgba(1,99,255,1) 100%);
+    background: linear-gradient(90deg, rgba(107,0,249,1) 0%, rgba(1,99,255,1) 100%);
+    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#6b00f9",endColorstr="#0163ff",GradientType=1);
+  }
+  `})]})}function h(r={}){const{wrapper:n}=Object.assign({},t(),r.components);return n?e.jsx(n,Object.assign({},r,{children:e.jsx(o,r)})):o(r)}export{h as default};
